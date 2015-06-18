a47c55baa423b6440d43e3b0e45b0955a3d8dd32
Sho Kodama <madakodama.1go@gmail.com>
2015-06-19 08:51:49 +0900

    新規記事投稿追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,21 @@ def new
   @post = Post.new
 end
 
+def create
+  @post = Post.new(post_params)
+  @post.save
+  #あとでやる
+  redirect_to"/posts/#{@spost.id}"
+end
+
+private
+
+def post_params
+  params.require(:post).permit(
+:title,
+:body,
+:category
+)
+
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+get '/posts/new' => 'posts#new'
+post '/posts/' => 'posts#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
